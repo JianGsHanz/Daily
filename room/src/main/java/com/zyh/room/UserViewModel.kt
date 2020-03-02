f8127265ac29dde30449beced05b4fcd982c9821
zhangyaohua <zhangyaohua@ithro.com.cn>
2020-03-02 22:23:47 +0800

    add room

--- room/src/main/java/com/zyh/room/UserViewModel.kt
+++ room/src/main/java/com/zyh/room/UserViewModel.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 /**
